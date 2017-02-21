8f66f2d0cac85769ad3f9dd54934914da32f12d8
Alvin Sovereign <alvin@alvsov.com>
2017-02-21 14:05:26 +0000

    Added placeholder Image and Blog Hero Image

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 import { Http } from '@angular/http';
 
@@ -14,15 +14,18 @@ import { PostDetailPage } from '../post-detail/post-detail';
 })
 export class HomePage {
 
-  // url: string = 'http://localhost:8888/wordpress/wp-json/wp/v2/posts';
-  url: string = 'http://www.arabakarsilastirma.org/wp-json/wp/v2/posts';
+  url: string = 'http://localhost:8888/wordpress/wp-json/wp/v2/posts';
+  // url: string = 'http://www.arabakarsilastirma.org/wp-json/wp/v2/posts';
   items: any;
+  page: any;
 
   constructor(public navCtrl: NavController, private http: Http) {
 
   }
 
   ionViewDidEnter() {
+    this.page = 1
+    
     this.http.get(this.url)
       .map(res => res.json())
       .subscribe(data => {
@@ -37,3 +40,4 @@ export class HomePage {
   }
 
 }
+
