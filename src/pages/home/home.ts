842093f8789e5814eecf5881cf52c82c24780785
Hamdi Ceylan <hamdiceylan@Hamdi-MacBook-Pro.local>
2017-02-27 21:33:23 +0000

    merge conflict

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,10 @@
+import { PostCommentsService } from './../../providers/post-comments';
 import { PostDetailPage } from './../../components/post-detail/post-detail';
 import { Wp } from './../../providers/wp';
 import { Http } from '@angular/http';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import 'rxjs/add/operator/map';
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-import { PostsService } from '../../providers/posts-service';
-import { PostDetailPage } from '../post-detail/post-detail';
->>>>>>> 59dc1c0120a34e4b426db63023175f45568539c1
-=======
->>>>>>> e5f8c3190db2587717bc849eaa6435e60f9b637b
 
 @Component({
   selector: 'page-home',
@@ -22,33 +14,16 @@ import { PostDetailPage } from '../post-detail/post-detail';
   templateUrl: 'home.html'
 })
 export class HomePage {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
   posts: any;
-=======
->>>>>>> 59dc1c0120a34e4b426db63023175f45568539c1
-=======
-
-  posts: any;
->>>>>>> e5f8c3190db2587717bc849eaa6435e60f9b637b
   page: any;
   items: any;
 
   constructor(
     public navCtrl: NavController,
-<<<<<<< HEAD
-<<<<<<< HEAD
     private http: Http,
     public wp: Wp) {
-=======
-    public postsService: PostsService) {
-
->>>>>>> 59dc1c0120a34e4b426db63023175f45568539c1
-=======
-    private http: Http,
-    public wp: Wp) {
->>>>>>> e5f8c3190db2587717bc849eaa6435e60f9b637b
   }
 
   ionViewDidEnter() {
@@ -58,11 +33,7 @@ export class HomePage {
     this.loadPosts();
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
   loadPosts() {
->>>>>>> e5f8c3190db2587717bc849eaa6435e60f9b637b
     this.wp.getPosts()
       .subscribe(posts => {
         posts.forEach(element => {
@@ -70,17 +41,6 @@ export class HomePage {
         });
         this.posts = posts;
     })
-<<<<<<< HEAD
-
-=======
-  loadPosts() {
-    this.postsService.getPosts()
-    .subscribe(response => {
-      this.items = response;
-    });
->>>>>>> 59dc1c0120a34e4b426db63023175f45568539c1
-=======
->>>>>>> e5f8c3190db2587717bc849eaa6435e60f9b637b
   }
 
   seeArticle(item) {
